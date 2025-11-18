afbd458c772ce447326772ee26fe77e3008a2678
Søren Palle Dyrbye <sod004@edu.zealand.dk>
2025-11-18 09:56:14 +0100

    wuf wuf

---
+++ ConsoleApp2/Hund.cs
@@ -0,0 +1,16 @@
+﻿
+
+    internal class Hund
+{
+    public string Race { get; set; }
+    public string Name { get; }
+
+    public int Age { get; set; }
+
+    public void noice ()
+    {
+        System.Console.WriteLine("Wuff Wuff");
+    }
+   
+}
+
