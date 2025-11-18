79b5f55998e7be4fcc819eae0e363adfb4ab81f1
Rasmus Madsen <ram006@edu.zealand.dk>
2025-11-18 10:23:03 +0100

    Added the IsGoodboy property to the hund class

--- ConsoleApp2/Hund.cs
+++ ConsoleApp2/Hund.cs
@@ -5,17 +5,19 @@ public class Hund
     public string Race { get; set; }
     public string Name { get; }
     public int Age { get; set; }
+    public bool IsGoodBoy { get; set; }
 
     public Hund (string name, string race, int age)
     {
         Name = name;
         Race = race;
         Age = age;
+        IsGoodBoy = true;
     }
     public void noice ()
     {
         System.Console.WriteLine("Wuff Wuff");
     }
-   
+
 }
 
