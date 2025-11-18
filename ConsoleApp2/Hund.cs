a929d4848606b9716fec60f5c464991996d147a5
Søren Palle Dyrbye <sod004@edu.zealand.dk>
2025-11-18 10:11:54 +0100

    hej2

--- ConsoleApp2/Hund.cs
+++ ConsoleApp2/Hund.cs
@@ -4,9 +4,14 @@ public class Hund
 {
     public string Race { get; set; }
     public string Name { get; }
-
     public int Age { get; set; }
 
+    public Hund (string name, string race, int age)
+    {
+        Name = name;
+        Race = race;
+        Age = age;
+    }
     public void noice ()
     {
         System.Console.WriteLine("Wuff Wuff");
