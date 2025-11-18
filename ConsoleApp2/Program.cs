a88fa5817ad7c7b24c1bc260754fba0bc6af9f33
Rasmus Madsen <ram006@edu.zealand.dk>
2025-11-18 10:46:27 +0100

    hello

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,7 +11,4 @@ Console.WriteLine($"Dog Age: {dog.Age}");
 
 Hund Sofus = new Hund("Sofus", "Dansk Svensk", 5);
 Console.WriteLine($"Dog Name: {Sofus.Name}");
-Sofus.noice();
-Console.WriteLine("Dette er en ændring lavet i github");
-Console.WriteLine($"Dog Name: {Sofus.Name}");
 Console.WriteLine(dog.IsTheDogAGoodBoy());
